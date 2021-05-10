6aa8b21091f40b9404c84cbb9a2b07f8a1a84ac9
Victoria Grinberg <31312528+rudimentum@users.noreply.github.com>
2021-05-10 19:10:20 +0300

    Handling of button click and using API

--- app/src/main/java/com/rudimentum/weatherapp/MainActivity.java
+++ app/src/main/java/com/rudimentum/weatherapp/MainActivity.java
@@ -2,13 +2,120 @@ package com.rudimentum.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    // variables for find xml-views
+    private EditText userField;
+    private Button mainBtn;
+    private TextView resultInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userField = findViewById(R.id.userField);
+        mainBtn = findViewById(R.id.mainBtn);
+        resultInfo = findViewById(R.id.resultInfo);
+        // handling of button click
+        mainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (userField.getText().toString().trim().equals("")) {
+                    Toast.makeText(MainActivity.this, R.string.empty_user_input, Toast.LENGTH_LONG).show();
+                } else {
+                    // user input
+                    String city = userField.getText().toString().trim();
+                    // API key
+                    String key = "4e2ab79173bd043b42a072e7d3f4d751";
+                    String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric";
+
+                    new URLData().execute(url);
+                }
+            }
+        });
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class URLData extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            resultInfo.setText(R.string.loading);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection httpURLConnection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.connect();
+
+                // get stream from connection
+                InputStream inputStream = httpURLConnection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line).append("\n");
+                }
+
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                assert httpURLConnection != null;
+                httpURLConnection.disconnect();
+                try {
+                    assert reader != null;
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject main = jsonObject.getJSONObject("main");
+                resultInfo.setText("Temperature: " + main.getDouble("temp") + "\n" + "Feels like: " + main.getDouble("feels_like"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
